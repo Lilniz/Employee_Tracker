490f9bc480d55b5e3fd5e44d30ee2c0ea675cad2
Lizzle <leefboettcher@gmail.com>
2022-08-29 01:43:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
 const connection = mysql.createPool({
-  host: "localhost",
-  database: "employee_db",
-  user: "root",
-  password: "Ifuonlyknewsome1!",
+    host: "localhost",
+    database: "employee_db",
+    user: "root",
+    password: "Ifuonlyknewsome1!",
 });
 
 const menu_start = () => {
@@ -17,14 +17,43 @@ const menu_start = () => {
             message: 'What would you like to do?',
             choices: [
                 'View all Departments',
-                'View all roles', 
-                'View all employees', 
-                'Add a department', 
-                'Add a role', 
-                'Add and employee', 
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add and employee',
                 'Update an employee role',
                 'Exit'
             ],
         }
-    ])
+    ]).then(response => {
+        switch (response.Action) {
+            case 'View all departments':
+                viewDepartment();
+                break;
+            case 'View all jobs':
+                viewJobs();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a job':
+                addJob();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update employee job':
+                updateEmployee();
+                break;
+            case "Exit":
+                connection.end();
+                break;
+            default:
+                connection.end();
+        }
+    })
 }
