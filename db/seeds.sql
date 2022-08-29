dd42ee558c4d0989946e097ed8943f0da614d44b
Lizzle <leefboettcher@gmail.com>
2022-08-29 01:39:50 -0500

    seed complete

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,25 @@
 INSERT INTO department (dept_name)
-
 VALUES
-('Parks and Recreation'),
-('Budget and Finance'),
-('City Planning');
+('East Rec'),
+('Project Space'),
+('Front of House');
 
 INSERT INTO job (title, salary, department_id)
 VALUES 
-('Director', 75000, 1),
-('Deputy Director', 45000, 1),
-('Assistant', 18000, 1),
-('Head Auditor', 100000, 2),
-('Accountant', 80000, 2),
-('Engineer', 55000, 3),
-('Miniature Horse', 0, 1);
+('CEO',             175000,     2),
+('President',       145000,     2),
+('Vice President',  118000,     2),
+('Rec Manager',     100000,     1),
+('Project Manager', 80000,      3),
+('Senior Planner',  65000,      1),
+('Junior House',    54000,      3);
 
 INSERT INTO employee (first_name, last_name, job_id, manager_id)
 VALUES 
-('Ron', 'Swanson', 1, NULL),
-('Leslie', 'Knope', 2, 1),
-('April', 'Ludgate', 3, 2),
-('Chris', 'Traeger', 4, NULL),
-('Ben', 'Wyatt', 5, 4),
-('Mark', 'Brendanawicz', 6, NULL),
-('Lil', 'Sebastian', 7, NULL);
+('Lee',     'Boettcher',    1,     null),
+('Kayla',   'Lime',         2,      1),
+('David',   'Quinn',        3,      2),
+('Gary',    'Anderson',     4,      null),
+('Beth',    'Wright',       5,      3),
+('Busy',    'Dawn',         6,      null),
+('Erick',   'Angel',        7,      null);
